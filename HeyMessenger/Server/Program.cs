b2e6b9f4d571876b9b337bbf354c25bd6fcd62b5
Baris Akin <m.akinbaris@gmail.com>
2018-02-06 08:59:47 +0100

    Verbindungsaufbau + Test

--- HeyMessenger/Server/Program.cs
+++ HeyMessenger/Server/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
 
 namespace Server
 {
@@ -10,6 +13,37 @@ namespace Server
     {
         static void Main(string[] args)
         {
+            IPAddress ip = Dns.GetHostEntry("localhost").AddressList[0];
+            TcpListener server = new TcpListener(ip, 8888);
+            TcpClient client = default(TcpClient);
+
+            try
+            {
+                server.Start();
+                Console.WriteLine("Server gestartet...");
+                Console.Read();
+            }
+            catch(Exception ex)
+            {
+                //Console.WriteLine(ex.ToString());
+                //Console.Read();
+                throw;
+            }
+            while(true)
+            {
+                client = server.AcceptTcpClient();
+
+                byte[] receivedBuffer = new byte[100];
+                NetworkStream stream = client.GetStream();
+
+                stream.Read(receivedBuffer, 0, receivedBuffer.Length);
+
+                string msg = Encoding.ASCII.GetString(receivedBuffer, 0, receivedBuffer.Length);  //Konvertierung Bytes zu String
+
+                Console.WriteLine(msg + receivedBuffer.Length);
+                Console.Read();
+                
+            }
         }
     }
 }
