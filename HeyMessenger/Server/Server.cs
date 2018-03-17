626d6705b24a21b8f9783c5eb7bbecd3f12b12ee
Baris Akin <m.akinbaris@gmail.com>
2018-03-17 14:23:56 +0100

    Kommentare verbessert 1.0

--- HeyMessenger/Server/Server.cs
+++ HeyMessenger/Server/Server.cs
@@ -6,49 +6,50 @@ using System.Threading.Tasks;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
-
+using System.Collections;
 
 namespace Server
 {
     class Server
     {
         /// <summary>
-        /// 
+        /// Server-Part
         /// </summary>
         /// <param name="args"></param>
+        public static Hashtable clientsList = new Hashtable();
         static void Main(string[] args)
         {
-            IPAddress ip = Dns.GetHostEntry("localhost").AddressList[0]; 
+            IPAddress ip = Dns.GetHostEntry("localhost").AddressList[0];
             TcpListener server = new TcpListener(IPAddress.Loopback, 4334);
             TcpClient client = default(TcpClient);
-            
+
             try //Versuch um Server zu starten
             {
                 //Thread.Sleep(30000);
                 server.Start(); //Server wird gestartet
                 Console.WriteLine("Server gestartet...");
                 //Console.Read();
-                
+
             }
-            catch(Exception ex) //Fehlerausgabe
+            catch (Exception ex) //Fehlerausgabe
             {
-                
+
                 Console.WriteLine(ex.ToString());
-               // Console.Read();
+                // Console.Read();
             }
-            
-            
-            while(true)
+
+
+            while (true)
             {
                 client = server.AcceptTcpClient();   //Akzeptiere TCP Client
-                
+
                 byte[] receivedBuffer = new byte[100];
                 NetworkStream stream = client.GetStream();
 
                 stream.Read(receivedBuffer, 0, receivedBuffer.Length);
 
                 StringBuilder msg = new StringBuilder();
-                
+
                 foreach (byte b in receivedBuffer)
                 {
                     if (b.Equals(59))
@@ -58,10 +59,37 @@ namespace Server
                     else
                         msg.Append(Convert.ToChar(b).ToString());
                 }
-                Console.WriteLine(msg.ToString()+ receivedBuffer.Length);
+                Console.WriteLine(msg.ToString() + receivedBuffer.Length);
+
+            }
+        }
+
+            public static void broadcast(string msg, string uName, bool flag) //Broadcast Funktion
+        {
+            
+            foreach (DictionaryEntry Item in clientsList)
+            {
                 
+                TcpClient broadcastSocket; //TCPClient für Broadcast
+                broadcastSocket = (TcpClient)Item.Value;
+                NetworkStream broadcastStream = broadcastSocket.GetStream();
+                Byte[] broadcastBytes = null;
+                
+                if (flag == true) //Übertragung
+                {
+                    broadcastBytes = Encoding.ASCII.GetBytes(uName + " says : " + msg);
+                }
+                else
+                {
+                    broadcastBytes = Encoding.ASCII.GetBytes(msg);
+                }
+                
+                broadcastStream.Write(broadcastBytes, 0, broadcastBytes.Length);
+                broadcastStream.Flush();
             }
             
+            //Beenden der Broadcast Funktion
+        
         }
     }
 }
